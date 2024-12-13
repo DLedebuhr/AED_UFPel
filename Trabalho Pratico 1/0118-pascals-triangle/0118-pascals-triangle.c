bfe8c363830f9d33022d2a78eb31953147a02ddc
Dyys <dbledebuhr@inf.ufpel.edu.br>
2024-12-13 19:11:23 -0300

    Atualização

--- Trabalho Pratico 1/0118-pascals-triangle/0118-pascals-triangle.c
+++ Trabalho Pratico 1/0118-pascals-triangle/0118-pascals-triangle.c
@@ -1,50 +1,31 @@
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
 int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
 
-    *returnSize = numRows; // tamanho da matriz
+    *returnSize = numRows; 
     
-    *returnColumnSizes = ( int * ) malloc ( numRows * sizeof(int) ); // e cada espaço da lista tem n colunos
+    *returnColumnSizes = ( int * ) malloc ( numRows * sizeof(int) ); 
 
-    int** matriz = ( int** ) malloc ( numRows * sizeof(int*) ); // [][] cria a matriz
+    int** matriz = ( int** ) malloc ( numRows * sizeof(int*) ); 
 
-    for (int i = 0; i < numRows; i++) { // linhas 
+    for (int i = 0; i < numRows; i++) { 
         
-        (*returnColumnSizes)[i] = i + 1; // tamanho da coluna atual
+        (*returnColumnSizes)[i] = i + 1; 
 
-        // i = 0  ->    1
-        // i = 1  ->    2
-        // i = 2  ->    3...
-
-        //   []
-        //  [][]
-        // [][][]...
+        matriz[i] = (int*) malloc ((i + 1) * sizeof(int));  
         
-        matriz[i] = (int*) malloc ((i + 1) * sizeof(int)); // linha atual 
-        
-        for (int j = 0; j <= i; j++) { // colunas
+        for (int j = 0; j <= i; j++) { 
 
             if (j == 0 || j == i) { 
             
-                matriz[i][j] = 1; //bordas
+                matriz[i][j] = 1; 
 
-                // j = 0 -> topo da coluna 1x1, logo matriz[0][0] = 1;
-            
             } else {
             
                 matriz[i][j] = matriz[i-1][j-1] + matriz[i-1][j]; 
-
-                // matriz [2][2] = matriz[1][1] + matriz[1][2] == 2;
-                                
-                                // matriz [1][1] = 1 linha e 0 coluna = 1
-                                // matriz [1][2] = 1 linha e 1 coluna = 1
             
             }
         }
     }
 
     return matriz;
+    
 }
